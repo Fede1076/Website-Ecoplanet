c4e8efac8babadd146d299b6f55ead26ecae9d4d
Federico López Londoño <fedelopez1076@gmail.com>
2023-12-04 21:23:12 -0500

    install laravel

--- acceso/acceso.js
+++ acceso/acceso.js
@@ -1,13 +1,20 @@
+//calendario
 document.addEventListener('DOMContentLoaded', function() {
     let calendarEl = document.getElementById('calendar');
     let calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
     locale:"es",
+    //button header
     headerToolbar:{
-        left:"prev,next today",
+        left:"prev",
         center:"title",
-        right:"BotonSolicitud"
+        right:"next"
     },
+    //button footer
+    footerToolbar:{
+        left:"BotonSolicitud today"
+    },
+    //boton para mostrar la ventana modal
     customButtons:{
         BotonSolicitud:{
             text:"Solicitar",
@@ -16,21 +23,39 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
     },
+    //funcion para agregar un evento dando click a una casilla
     dateClick:function(info){
         $("#modalAccess").modal("show");
+        console.log(info);
+        calendar.addEvent({title:"evento x", date:info.dateStr});
     },
+    //funcion para mostrar datos en consola 
+    eventClick:function(info) {
+        console.log(info);
+        console.log(info.event.title);
+        console.log(info.event.start);
+
+        console.log(info.event.end);
+        console.log(info.event.textColor);
+        console.log(info.event.backgroundColor);
+        //console.log para mostrar un evento extendido que no es propio de fullcalendar
+        console.log(info.event.extendedProps.descripcion);
+    },
+    //eventos agregados al calendario
     events:[
         {
             title:"Solicitar dia",
             start:"2023-12-10 12:30:00",
             end:"2023-12-13 12:30:00",
             color:"#FFCCAA",
-            textColor:"#000000"
+            textColor:"#000000",
+            descripcion:"descripción evento"
         },{
 
         }
     ]
     });
+    //renderizado del calendario en la pantalla
     calendar.render();
 });
 
